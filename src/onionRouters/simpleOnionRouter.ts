f9f5d89de0cebd57ff2ef2ce1e8f556b8140edce
michel aurier <michel.aurier@edu.devinci.fr>
2024-03-04 14:56:59 +0100

    Step 1.1 to 1.3

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -3,20 +3,21 @@ import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
 
 export async function simpleOnionRouter(nodeId: number) {
-  const onionRouter = express();
-  onionRouter.use(express.json());
-  onionRouter.use(bodyParser.json());
+    const onionRouter = express();
+    onionRouter.use(express.json());
+    onionRouter.use(bodyParser.json());
 
-  // TODO implement the status route
-  // onionRouter.get("/status", (req, res) => {});
+    onionRouter.get("/status", (req, res) => {
+        res.send("live");
+    });
 
-  const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
-    console.log(
-      `Onion router ${nodeId} is listening on port ${
-        BASE_ONION_ROUTER_PORT + nodeId
-      }`
-    );
-  });
+    const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
+        console.log(
+            `Onion router ${nodeId} is listening on port ${
+                BASE_ONION_ROUTER_PORT + nodeId
+            }`
+        );
+    });
 
-  return server;
+    return server;
 }
--- src/users/user.ts
+++ src/users/user.ts
@@ -2,22 +2,18 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_USER_PORT } from "../config";
 
-export type SendMessageBody = {
-  message: string;
-  destinationUserId: number;
-};
-
 export async function user(userId: number) {
   const _user = express();
   _user.use(express.json());
   _user.use(bodyParser.json());
 
-  // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
+  _user.get("/status", (req, res) => {
+    res.send("live");
+  });
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
-      `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
+        `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
     );
   });
 
